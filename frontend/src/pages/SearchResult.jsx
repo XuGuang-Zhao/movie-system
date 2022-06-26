5b1e89ce175026293c50cd36f160164b7413ca8a
Anthony_Zhao <1249632746@qq.com>
2022-06-26 15:21:18 +0800

    fix bug

--- frontend/src/pages/SearchResult.jsx
+++ frontend/src/pages/SearchResult.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const SearchResult = () => {
     const movieList = JSON.parse(localStorage.getItem('movie_list')) || '';
     const filterList = JSON.parse(localStorage.getItem('filter_list')) || '';
+    console.log(movieList);
+    console.log(filterList);
     return (
         <div>
             This is Search Result Page!
